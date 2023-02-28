e11db83cd18b6e8e5abb0e16c9e3b548e5be433e
Alexander Yoshino <alexander.yoshino@mail.utoronto.ca>
2023-02-28 04:05:11 -0500

    updated the compare and local command to use version # of sessions

--- cli/main.py
+++ cli/main.py
@@ -22,15 +22,24 @@ def login(loader: Instaloader, username: str, password: str) -> None:
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Perform analysis on instagram accounts.")
-    parser.add_argument('-s', '--setup', help="Run with -s to setup authorization.", action='store_true')
-    parser.add_argument('-u', '--username', help="Set the username to run with.", action='store', dest='username', required=True)
-    parser.add_argument('-p', '--password', help="Set the password to run with.", action='store', dest='password')
-    parser.add_argument('-t', '--target', help="Set the target to run against.", action='store', dest='target')
-    parser.add_argument('-o', '--output', help="Set the output file to write to.", action='store', dest='output')
-    parser.add_argument('-c', '--compare', help="Compare an old pickle to a new username", action='store_true')
-    parser.add_argument('-l', '--local', help="Load a pickle file", action='store_true', dest='local')
-    parser.add_argument('-date', '--date', help="Check the dates of the pickle files currently stored", action='store_true', dest='date')
+    parser = argparse.ArgumentParser(
+        description="Perform analysis on instagram accounts.")
+    parser.add_argument(
+        '-s', '--setup', help="Run with -s to setup authorization.", action='store_true')
+    parser.add_argument('-u', '--username', help="Set the username to run with.",
+                        action='store', dest='username', required=True)
+    parser.add_argument(
+        '-p', '--password', help="Set the password to run with.", action='store', dest='password')
+    parser.add_argument(
+        '-t', '--target', help="Set the target to run against.", action='store', dest='target')
+    parser.add_argument(
+        '-o', '--output', help="Set the output file to write to.", action='store', dest='output')
+    parser.add_argument('-c', '--compare', help="Compare an old pickle to a new username, input the revision number or 0 for no revision number",
+                        action='store', dest='compare')
+    parser.add_argument('-l', '--local', help="Load a pickle file",
+                        action='store', dest='local')
+    parser.add_argument(
+        '-date', '--date', help="Check the dates of the pickle files currently stored", action='store_true', dest='date')
     parser.add_argument('-n', '--omit')
     args = parser.parse_args()
     loader = instaloader.Instaloader()
@@ -38,31 +47,37 @@ def main():
         if os.path.exists('pickles'):
             for file in os.listdir('pickles'):
                 if file.endswith('.pickle'):
-                    with open (f'pickles/{file}', 'rb') as f:
-                        print(f'File: {file} - Date: {pickle.load(f).updated_at}')
+                    with open(f'pickles/{file}', 'rb') as f:
+                        print(
+                            f'File: {file} - Date: {pickle.load(f).updated_at}')
     if args.setup:
         login(loader, args.username, args.password)
     else:
         try:
-            loader.load_session_from_file(args.username, filename=f'sessions/session-{args.username}')
+            loader.load_session_from_file(
+                args.username, filename=f'sessions/session-{args.username}')
         except FileNotFoundError:
             print("Cannot find session file. Run with -s to setup authorization.")
             return
     if args.compare and args.target:
-        with open(f'pickles/{args.target}.pickle', 'rb') as f:
+        with open(f'pickles/{args.target}{"_" + args.compare if args.compare != 0 else ""}.pickle', 'rb') as f:
             old = pickle.load(f)
         # look at pickle info
-        target = ProfileContainer(args.target, loader)
-        target.get_posts()
-        target.get_followers()
-        target.get_following()
-        target.get_likes()
+        if args.local:
+            with open(f'pickles/{args.target}{"_" + args.local if args.local != 0 else ""}.pickle', 'rb') as f:
+                target = pickle.load(f)
+        else:
+            target = ProfileContainer(args.target, loader)
+            target.get_posts()
+            target.get_followers()
+            target.get_following()
+            target.get_likes()
+            target.serialize()
         target.compare(old)
-        target.serialize()
 
     elif args.target:
         if args.local:
-            with open(f'pickles/{args.target}.pickle', 'rb') as f:
+            with open(f'pickles/{args.target}{"_" + args.local if args.local != 0 else ""}.pickle', 'rb') as f:
                 target = pickle.load(f)
         else:
             target = ProfileContainer(args.target, loader)
@@ -72,6 +87,3 @@ def main():
             target.get_likes()
             target.serialize()
         target.write_to_excel()
-
-
-
